22808e12fa6b952a56bcc930b69e64333ce74128
Airwear <airwearsport@gmail.com>
2025-10-20 03:09:14 -0400

    chore: remove BOM from JSON/config files for EAS build

--- scripts/fix-expo-autolinking.js
+++ scripts/fix-expo-autolinking.js
@@ -1,4 +1,4 @@
-﻿const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 const dest = path.join(__dirname, '..', 'node_modules', 'expo-modules-autolinking', 'android', 'expo-gradle-plugin');
 if (!fs.existsSync(dest)) {
